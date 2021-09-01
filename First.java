31e7e06c32d535917e09613b96c4534408af93fa
seanprogrammingbrundage <89647834+seanprogrammingbrundage@users.noreply.github.com>
2021-09-01 11:12:26 -0400

    Add files via upload
    first project

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World");
+    for(String word : args)
+        System.out.println(args);
+    }
+}
